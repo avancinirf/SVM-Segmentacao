2ac1512d24f15cfd2e8edcd1fac7f626a6bb691c
Ricardo Avancini <avancini.rf@gmail.com>
2017-08-19 02:56:24 -0300

    Início do projeto, foi criado apenas um script inicial e instalado o pacote e1071.

---
+++ SVM_01.R
@@ -0,0 +1,24 @@
+
+### --------------------------------------------------------
+### SVM - Suporte Vector Machine - Segmentação de imagem
+### Tutorial adaptado de: https://2engenheiros.wordpress.com/2017/07/24/classificar-dados-svm-no-r/
+### --------------------------------------------------------
+
+### --------------------------------------------------------
+### Preparação dos dados
+### --------------------------------------------------------
+
+# Pacote snecessários
+install.packages("e1071")
+library("e1071")
+attach(iris)
+
+
+
+
+
+
+
+
+
+
